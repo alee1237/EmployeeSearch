3d12ad362ba9b9ca390c92a4da45c6db135b739d
Andrew Lee <andrewklee@cpp.edu>
2023-10-26 21:53:52 -0700

    implement other two search functions

--- employeemain.c
+++ employeemain.c
@@ -5,21 +5,22 @@
 int main() {
     EmployeePtr searchEmployeeByNumber(const Employee table[], int tableSize, long numberToFind);
     EmployeePtr searchEmployeeByName(const Employee table[], int tableSize, char* nameToFind);
+    EmployeePtr searchEmployeeByPhone(const Employee table[], int tableSize, char* phoneToFind);
+    EmployeePtr searchEmployeeBySalary(const Employee table[], int tableSize, double salaryToFind);
 
-    extern Employee EmployeeTable[];
-    extern const int EmployeeTableEntries;
+    extern Employee employeeTable[];
+    extern const int employeeTableEntries;
 
-    EmployeePtr matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045);
-    
+    EmployeePtr matchPtr = searchEmployeeByNumber(employeeTable, employeeTableEntries, 1045);
     if (matchPtr != NULL) {
-        printf("Employee ID 1045 is in record %d\n", matchPtr - EmployeeTable);
+        printf("Employee ID 1045 is in record %d\n", matchPtr - employeeTable);
     } else {
         printf("Employee ID is NOT found in the record\n");
     }
 
-    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+    matchPtr = searchEmployeeByName(employeeTable, employeeTableEntries, "Tony Bobcat");
     if (matchPtr != NULL) {
-        printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable);
+        printf("Employee Tony Bobcat is in record %d\n", matchPtr - employeeTable);
     } else {
         printf("Employee Tony Bobcat is NOT found in the record\n");
     }
--- employeeone.c
+++ employeeone.c
@@ -20,3 +20,21 @@ EmployeePtr searchEmployeeByName(ConstEmployeePtr ptr, int tableSize, char* targ
     }
     return NULL;
 }
+
+EmployeePtr searchEmployeeByPhone(ConstEmployeePtr ptr, int tableSize, char* targetPhone) {
+    const ConstEmployeePtr endPtr = ptr + tableSize;
+    for (; ptr < endPtr; ptr++) {
+        if (strcmp(ptr -> phone, targetPhone) == 0) {
+            return (EmployeePtr) ptr;
+        }
+    }
+}
+
+EmployeePtr searchEmployeeBySalary(ConstEmployeePtr ptr, int tableSize, double targetSalary) {
+    const ConstEmployeePtr endPtr = ptr + tableSize;
+    for (; ptr < endPtr; ptr++) {
+        if (ptr -> salary == targetSalary) {
+            return (EmployeePtr) ptr;
+        }
+    }
+}
