6e0138dcebcce38be20f861a344a4ca7c945277a
Andrew Lee <andrewklee@cpp.edu>
2023-10-26 23:09:42 -0700

    implement function pointers

--- employeetwo.c
+++ employeetwo.c
@@ -20,11 +20,27 @@ static int compareEmployeeName(const void *targetPtr, ConstEmployeePtr ptr) {
     return strcmp((char*) targetPtr, ptr -> name);
 }
 
+static int compareEmployeePhone(const void *targetPtr, ConstEmployeePtr ptr) {
+    return strcmp((char*) targetPtr, ptr -> phone);
+}
+
+static int compareEmployeeSalary(const void *targetPtr, ConstEmployeePtr ptr) {
+    return * (double*) targetPtr != ptr -> salary; // typecast then dereference
+}
+
 // these are wrappers, used in main
 EmployeePtr searchaEmployeeByNumber(ConstEmployeePtr ptr, int size, long number) {
     return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
 }
 
-EmployeePtr searchEmploayeeByName(ConstEmployeePtr ptr, int size, char* name) {
+EmployeePtr searchEmployeeByName(ConstEmployeePtr ptr, int size, char* name) {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+EmployeePtr searchEmployeeByPhone(ConstEmployeePtr ptr, int size, char* phone) {
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+
+EmployeePtr searchEmployeeBySalary(ConstEmployeePtr ptr, int size, double salary) {
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
